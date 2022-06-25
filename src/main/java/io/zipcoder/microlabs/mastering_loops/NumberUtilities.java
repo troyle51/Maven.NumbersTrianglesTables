bed7864e55ad3eb7b284f295ba1966f979ecad61
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-06-25 15:19:04 -0400

    completed odd, even, and square

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,17 +3,39 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String outcome = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 == 0) {
+                System.out.println(i);
+                outcome += i;
+            }
+
+        }
+        return outcome;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String outcome = "";
+        for (int i = start; i < stop; i++){
+            if (i % 2 != 0) {
+                System.out.println(i);
+                outcome += i;
+            }
+
+        }
+        return outcome;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String outcome = "";
+        for (int i = start; i < stop; i+=5){
+            int b = i * i;
+            System.out.println(b);
+            outcome += b;
+        }
+        return outcome;
     }
 
     public static String getRange(int start) {
