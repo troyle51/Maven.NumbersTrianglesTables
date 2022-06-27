ba35574be2f5450e12696cd2afc2baa669e9011a
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-06-26 20:30:01 -0400

    Completed tables

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -16,22 +16,24 @@ public class TableUtilities {
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
-    }
-
-    public static String getMultiplicationTable(int tableSize) {
-        return null;
-    }
-
-    public static String printRow(int a) {
         String outcome = "";
-        int i = a;
-        while (i <= a) {
-            outcome = outcome + String.format("%4d", i);
-            i = i + 1;
+        for(int i = 1; i < 11; i++){
+            for(int j = 1; j < 11; j++){
+                outcome += String.format("%3d |",i * j);
+            }
+            outcome += "\n";
         }
         return outcome;
     }
 
-
+    public static String getMultiplicationTable(int tableSize) {
+        String outcome = "";
+        for(int i = 1; i < tableSize+1; i++){
+            for(int j = 1; j < tableSize+1; j++){
+                outcome += String.format("%3d |",i * j);
+            }
+            outcome += "\n";
+        }
+        return outcome;
+    }
 }
